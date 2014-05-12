0e8bf0ff05961e852385194b1da9886a2d635e1b
Oleksandr Khomenko <okhomenko@gmail.com>
2014-05-12 19:30:54 +0300

    Stubs for view and collection tests

---
+++ test/collection.js
@@ -0,0 +1,16 @@
+// should fetch collection from server
+// should call success on fetch
+
+describe('Collection', function () {
+    'use strict';
+    var Collection;
+
+    beforeEach(function () {
+        Collection = window.Collection;
+    });
+
+    it('should fetch via ajax');
+
+    it('should call callback on success');
+
+});
---
+++ test/view.js
@@ -0,0 +1,28 @@
+// should render template to @el
+// should exec callback on finish render
+// should call collection.fetch
+// should populate on collection.fetch success
+
+describe('View', function () {
+    'use strict';
+    var View;
+
+    beforeEach(function () {
+        View = window.View;
+    });
+
+    describe('@render', function () {
+        it('should render template');
+
+        it('should exec callback');
+    });
+
+    describe('@populate', function () {
+        it('should call collection.fetch');
+
+        it('should populate @el on collection.fetch success');
+    });
+
+
+
+});
